48e7bf5ae6bbade99ce94ab6432d32f8b996932c
Himanshu Singh <himanshu.singh5862@gmail.com>
2026-05-23 19:53:22 +0530

    Changes made in ProductViewer Section

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -19,7 +19,7 @@ const ProductViewer = () => {
         <h2>Take a closer look</h2>
 
         <div className='controls'>
-            <p className='info'>Macbook {scale} available in {color}</p>
+            <p className='info'>Macbook Pro | available in 14" & 16" in Space Gray & Dark colors</p>
 
             <div className="flex-center gap-5 mt-5">
 
