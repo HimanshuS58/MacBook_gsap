2405f0ada3c719545076a6546921d9422711589b
Himanshu Singh <himanshu.singh5862@gmail.com>
2026-05-23 19:51:04 +0530

    Implement 3D Product Viewer Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import NavBar from './components/NavBar'
 import Hero from './components/Hero'
+import ProductViewer from './components/ProductViewer'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'  // To animate elements based on scroll position.
+
+
+gsap.registerPlugin(ScrollTrigger)
 
 const App = () => {
   return (
     <main>
       <NavBar />
       <Hero />
+      <ProductViewer />
     </main>
   )
 }
