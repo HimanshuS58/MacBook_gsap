96d892ef09ecceb6c6703d7e01a6f14cbecfb511
Himanshu Singh <himanshu.singh5862@gmail.com>
2026-05-24 14:18:48 +0530

    implement showcase section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './components/Hero'
 import ProductViewer from './components/ProductViewer'
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/all'  // To animate elements based on scroll position.
+import Showcase from './components/Showcase'
 
 
 gsap.registerPlugin(ScrollTrigger)
@@ -14,6 +15,7 @@ const App = () => {
       <NavBar />
       <Hero />
       <ProductViewer />
+      <Showcase />
     </main>
   )
 }
